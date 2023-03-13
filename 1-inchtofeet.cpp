195a86ea58d61a145ba27a8216da1e7eae70e8f9
nwogu kennedy <savvy@MAKIMA.localdomain>
2023-03-13 02:44:11 +0100

    convert height from inches to feet

---
+++ 1-inchtofeet.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+/**
+ * main - function that returns the length in feet
+ * @conv: constant for converting from inch to feet
+ * @inches: the length in inches
+ * @feet: the height in feet
+ * @rem: the remainder which will be written in inches
+ * return: Always 0
+ */
+
+using namespace std;
+
+int main(void)
+{
+	const double conv = 0.08333333333333333;
+	int inches;
+	double feet, rem;
+
+	cout << "Enter height in inches here_";
+	cin >> inches;
+
+	feet = inches * conv;
+	rem = feet - int (feet);
+	cout << "height is " << int (feet)\
+		<< " feet and " << float (rem) << " inches";
+	return (0);
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Random C/C++ projects as I learn
